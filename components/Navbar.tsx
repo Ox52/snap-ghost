75475d6933270c2e479a75aa3875915ebbdf7874
Ox52 <harsh04241@gmail.com>
2025-12-15 04:27:53 +0530

    responsive navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,13 @@
+"use client"
 import Link from 'next/link'
 import Image from "next/image";
-import React from 'react'
+import React, { useState } from 'react'
 import SnapNinjaLogo from './SnapNinjaLogo';
+import { Menu,X } from 'lucide-react';
 
 export default function Navbar() {
+
+    const [open,setOpen] =useState(false)
   return (
     <div className="fixed right-0 left-0 z-50 border-white/10 border-b  bg-stone-950/90 backdrop-blur-md">
       <div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
@@ -24,7 +28,7 @@ export default function Navbar() {
             </span>
           </Link>
 
-          <nav className="flex items-center space-x-8 md:flex">
+          <nav className=" hidden flex items-center space-x-8 md:flex">
             <div className="flex items-center space-x-2  rounded-full border border-green-500/10  bg-green-500/10  px-3 py-1 ">
               <div className="h-2 w-2 animate-pulse rounded-full  bg-green-500/10 "></div>
 
@@ -52,7 +56,46 @@ export default function Navbar() {
               Downlaod Now
             </button>
           </nav>
+          <div className="md:hidden  w-8 h-8 items-center justify-center">
+            {open ? (
+              <X className="cursor-pointer" onClick={() => setOpen(false)} />
+            ) : (
+              <Menu className="cursor-pointer" onClick={() => setOpen(true)} />
+            )}
+          </div>
         </div>
+
+        {open && (
+          <div className="md:hidden bg-stone-950/95 backdrop-blur-md border-t border-white/10 py-7">
+            <div className=" flex flex-col gap-4  py-6">
+              <div className="flex items-center  rounded-full border border-green-500/10  bg-green-500/10 gap-2  px-2 py-1  w-fit ">
+                <div className="h-2 w-2 animate-pulse rounded-full  bg-green-500/10 "></div>
+                <div className="font-mono font-semibold tracking-wider text-xs  text-green-400">
+                  {" "}
+                  UNDECECTED
+                </div>
+              </div>
+              <Link
+                href="/Features"
+                className="text-[#FFFC00] font-semibold text-sm"
+                onClick={() => setOpen(false)}
+              >
+                Features
+              </Link>
+
+              <Link
+                href="/Pricing"
+                className="text-gray-300 hover:text-[#FFFC00] font-semibold text-sm"
+                onClick={() => setOpen(false)}
+              >
+                Pricing
+              </Link>
+              <button className="bg-white mt-6 -mb-5 px-6 py-2 text-black font-bold hover:bg-[#FFFC00]">
+                Download Now
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
